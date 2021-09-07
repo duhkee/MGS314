1e8be371c0cb058fcc3e2bcd725d08e96d09e512
shrutikg <47606384+shrutikg@users.noreply.github.com>
2021-09-07 19:53:36 -0400

    Add files via upload

---
+++ MGS 314 Lab 1.py
@@ -0,0 +1,16 @@
+a = 21/7
+print(a)
+
+print(type(a))
+
+points_obtained = 95
+total_points = 100
+percentage = points_obtained/total_points*100
+print(int(percentage))
+
+abc = "abcdef"
+print(abc[2])
+print(len(abc))
+print(abc[-1])
+
+add(2,3)
